9d0afc739c11e0db58c61f01ae2cc179a9ca8372
vpol <vpol@kth.se>
2017-01-07 04:37:26 +0100

    Added some function definitions

---
+++ src/data_handling.py
@@ -0,0 +1,9 @@
+
+def preprocess_data(data):
+    """
+    :param data: list of pairs (document, labels)
+    :return: data in the same format
+    """
+    # todo remove stop-words, punctuation
+
+    return data
