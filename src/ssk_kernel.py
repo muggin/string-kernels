d8dc5bf4dad7ca95f2bd2bb166e7627417528935
Wojtek Kryscinski <wkryscinski@gmail.com>
2017-01-13 12:42:14 +0100

    Bug fixes in SSK

--- src/ssk_kernel.py
+++ src/ssk_kernel.py
@@ -7,21 +7,10 @@ from string import lowercase
 
 
 def naive_ssk_kernel(s, t, k, l):
-    """
-    Naive implementation
-
-    :param x: first string
-    :param y: second string
-    :param k: length
-    :param l: lambda
-    :return: SSK distance between two strings given parameters
-    """
-
     kernel_sum = 0
     for subseq in iter.permutations(lowercase + ' ', k):
         for idc_s in _find_all_subsequence_indices(subseq, s):
             for idc_t in _find_all_subsequence_indices(subseq, t):
-                print subseq, s, idc_s, t, idc_t
                 kernel_sum += math.pow(l, _subsequence_length(idc_s)+_subsequence_length(idc_t))
     return kernel_sum
 
@@ -45,48 +34,55 @@ def _find_all_char_indices(char, string):
     return [idx for idx, ltr in enumerate(string) if ltr == char]
 
 
-def compute_K_prime(s, t, k, l):
-    K_prime = np.ones((k-1, len(s), len(t)))
+def _compute_K_prime(s, t, k, l):
+    K_prime = np.ones((k, len(s)+1, len(t)+1))
 
-    for i in xrange(1, k-1):
-        for m in xrange(len(s)):
-            for n in xrange(len(t)):
+    for i in xrange(1, k):
+        for m in xrange(len(s)+1):
+            for n in xrange(len(t)+1):
                 if min(m, n) < i:
                     K_prime[i][m][n] = 0
                     continue
 
                 K_prime[i][m][n] = l*K_prime[i][m-1][n] + \
-                       sum([K_prime[i-1][m-1][j-1] * l**(len(t)-j+2) for j in _find_all_char_indices(s[-1], t)])
+                       sum([K_prime[i-1][m-1][j] * l**(len(t)-j+2) for j in _find_all_char_indices(s[m-1], t)])
 
     return K_prime
 
 
-def ssk_kernel(s, t, k, l, K_prime):
+def _ssk_kernel(s, t, k, l, K_prime):
     if min(len(s), len(t)) < k:
         return 0
 
-    print s, t, _find_all_char_indices(s[-1], t)
-    return ssk_kernel(s[:-1], t, k, l, K_prime) + \
-           l**2 * sum([K_prime[k-2][len(s)-1][j-1] for j in _find_all_char_indices(s[-1], t)])
+    return _ssk_kernel(s[:-1], t, k, l, K_prime) + \
+           l**2 * sum([K_prime[k-1][len(s)-1][j] for j in _find_all_char_indices(s[-1], t)])
+
+
+def ssk_kernel(s, t, k, l, K_prime):
+    K_prime = _compute_K_prime(str_a, str_b, k, l)
 
 if __name__ == '__main__':
-    str_a = 'science is organized knowledge'
-    str_b = 'wisdom is organized life'
-
-    str_a = 'cats'
-    str_b = 'camats'
+    test_set = [
+        ('cats', 'cats'),
+        ('hats', 'cats'),
+        ('wojtek', 'wojciech'),
+        ('science', 'knowledge'),
+        ('science is organized knowledge', 'wisdom is organized life')
+    ]
 
     k = 3
-    l = 0.95
-    K_prime = compute_K_prime(str_a, str_b, k, l)
-    print K_prime.shape
-    ssk_ab = ssk_kernel(str_a, str_b, k, l, K_prime)
-    ssk_a = ssk_kernel(str_a, str_a, k, l, K_prime)
-    ssk_b = ssk_kernel(str_b, str_b, k, l, K_prime)
-    print 'AB:', ssk_ab, 'A:', ssk_a, 'B:', ssk_b, 'NORM', ssk_ab / math.sqrt(ssk_a * ssk_b)
+    l = 0.9
+    for str_a, str_b in test_set:
+        print '{} ~ {}'.format(str_a, str_b)
+        K_prime = _compute_K_prime(str_a, str_b, k, l)
+        ssk_ab = _ssk_kernel(str_a, str_b, k, l, K_prime)
+        K_prime = _compute_K_prime(str_a, str_a, k, l)
+        ssk_a = _ssk_kernel(str_a, str_a, k, l, K_prime)
+        K_prime = _compute_K_prime(str_b, str_b, k, l)
+        ssk_b = _ssk_kernel(str_b, str_b, k, l, K_prime)
+        print 'AB:', ssk_ab, 'A:', ssk_a, 'B:', ssk_b, 'NORM', ssk_ab / math.sqrt(ssk_a * ssk_b)
 
-    ssk_ab = naive_ssk_kernel(str_a, str_b, k, l)
-    print 'After AB'
-    ssk_a = naive_ssk_kernel(str_a, str_a, k, l)
-    ssk_b = naive_ssk_kernel(str_b, str_b, k, l)
-    print 'AB:', ssk_ab, 'A:', ssk_a, 'B:', ssk_b, 'NORM', ssk_ab / math.sqrt(ssk_a * ssk_b)
+        ssk_ab = naive_ssk_kernel(str_a, str_b, k, l)
+        ssk_a = naive_ssk_kernel(str_a, str_a, k, l)
+        ssk_b = naive_ssk_kernel(str_b, str_b, k, l)
+        print 'AB:', ssk_ab, 'A:', ssk_a, 'B:', ssk_b, 'NORM', ssk_ab / math.sqrt(ssk_a * ssk_b), '\n'
